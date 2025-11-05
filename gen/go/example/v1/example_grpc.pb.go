82b1e8ab8a371725378f435836ecb80a24ccb805
Nikita Rakcheev <rakcheev@intervolga.ru>
2025-11-05 21:44:32 +0300

    PS-2: Test2 Controller.

--- gen/go/example/v1/example_grpc.pb.go
+++ gen/go/example/v1/example_grpc.pb.go
@@ -19,7 +19,8 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	ExampleService_Hello_FullMethodName = "/example.v1.ExampleService/Hello"
+	ExampleService_Hello_FullMethodName  = "/example.v1.ExampleService/Hello"
+	ExampleService_Hello2_FullMethodName = "/example.v1.ExampleService/Hello2"
 )
 
 // ExampleServiceClient is the client API for ExampleService service.
@@ -27,6 +28,7 @@ const (
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type ExampleServiceClient interface {
 	Hello(ctx context.Context, in *HelloRequest, opts ...grpc.CallOption) (*HelloResponse, error)
+	Hello2(ctx context.Context, in *HelloRequest2, opts ...grpc.CallOption) (*HelloResponse2, error)
 }
 
 type exampleServiceClient struct {
@@ -47,11 +49,22 @@ func (c *exampleServiceClient) Hello(ctx context.Context, in *HelloRequest, opts
 	return out, nil
 }
 
+func (c *exampleServiceClient) Hello2(ctx context.Context, in *HelloRequest2, opts ...grpc.CallOption) (*HelloResponse2, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(HelloResponse2)
+	err := c.cc.Invoke(ctx, ExampleService_Hello2_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // ExampleServiceServer is the server API for ExampleService service.
 // All implementations must embed UnimplementedExampleServiceServer
 // for forward compatibility.
 type ExampleServiceServer interface {
 	Hello(context.Context, *HelloRequest) (*HelloResponse, error)
+	Hello2(context.Context, *HelloRequest2) (*HelloResponse2, error)
 	mustEmbedUnimplementedExampleServiceServer()
 }
 
@@ -64,6 +77,9 @@ type UnimplementedExampleServiceServer struct{}
 
 func (UnimplementedExampleServiceServer) Hello(context.Context, *HelloRequest) (*HelloResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Hello not implemented")
+}
+func (UnimplementedExampleServiceServer) Hello2(context.Context, *HelloRequest2) (*HelloResponse2, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Hello2 not implemented")
 }
 func (UnimplementedExampleServiceServer) mustEmbedUnimplementedExampleServiceServer() {}
 func (UnimplementedExampleServiceServer) testEmbeddedByValue()                        {}
@@ -104,6 +120,24 @@ func _ExampleService_Hello_Handler(srv interface{}, ctx context.Context, dec fun
 	return interceptor(ctx, in, info, handler)
 }
 
+func _ExampleService_Hello2_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(HelloRequest2)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ExampleServiceServer).Hello2(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: ExampleService_Hello2_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ExampleServiceServer).Hello2(ctx, req.(*HelloRequest2))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // ExampleService_ServiceDesc is the grpc.ServiceDesc for ExampleService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -114,6 +148,10 @@ var ExampleService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Hello",
 			Handler:    _ExampleService_Hello_Handler,
+		},
+		{
+			MethodName: "Hello2",
+			Handler:    _ExampleService_Hello2_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
